246a9ff73bf00d50f990b1d8ba47bd62413fbd5d
Youssef Victor <youssefvictor00@gmail.com>
2021-03-10 21:20:57 +0200

    Initial project structure (#4)
    * Initial project structure

---
+++ src/LinkValidator/LinkValidatorCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MarkdownLinksVerifier.LinkClassifier;
+
+namespace MarkdownLinksVerifier.LinkValidator
+{
+    // TODO: Have a caching mechanism.
+    internal static class LinkValidatorCreator
+    {
+        public static ILinkValidator Create(LinkClassification classification, string baseDirectory)
+            => classification switch
+            {
+                LinkClassification.Online => new OnlineLinkValidator(),
+                LinkClassification.Local => new LocalLinkValidator(baseDirectory),
+                _ => throw new ArgumentException($"Invalid {nameof(classification)}.", nameof(classification))
+            };
+    }
+}
---
+++ src/LinkValidator/LocalLinkValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace MarkdownLinksVerifier.LinkValidator
+{
+    internal class LocalLinkValidator : ILinkValidator
+    {
+        private string _baseDirectory;
+
+        public LocalLinkValidator(string baseDirectory) => _baseDirectory = baseDirectory;
+
+        public bool IsValid(string link)
+            => File.Exists(Path.GetRelativePath(relativeTo: _baseDirectory, path: link));
+    }
+}
