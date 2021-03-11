f3dd89007403b14d0a2c75d9c997a586c103813d
Youssef Victor <youssefvictor00@gmail.com>
2021-03-11 07:57:12 +0200

    Fix ArgumentException for empty path (#18)

--- src/LinkValidator/LocalLinkValidator.cs
+++ src/LinkValidator/LocalLinkValidator.cs
@@ -10,6 +10,12 @@ namespace MarkdownLinksVerifier.LinkValidator
 
         public bool IsValid(string link)
         {
+            // Consider [text]() as valid. It redirects to the current directory.
+            if (string.IsNullOrEmpty(link))
+            {
+                return true;
+            }
+
             var path = Path.GetRelativePath(relativeTo: _baseDirectory, path: link);
             return File.Exists(path) || Directory.Exists(path);
         }
