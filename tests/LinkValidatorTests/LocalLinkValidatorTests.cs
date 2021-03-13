e25c4238d4b3192f4b28a5f76225ae83af21099a
Youssef Victor <youssefvictor00@gmail.com>
2021-03-13 17:52:41 +0200

    Initial work for unit tests (#44)
    * Initial work for unit tests
    
    * More work
    
    * Fix platform-specific failure

--- tests/LinkValidatorTests/LocalLinkValidatorTests.cs
+++ tests/LinkValidatorTests/LocalLinkValidatorTests.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
 
 namespace MarkdownLinksVerifier.UnitTests.LinkValidatorTests
 {
     public class LocalLinkValidatorTests
     {
+        private static async Task<int> WriteResultsAndGetExitCodeAsync(StringWriter writer)
+            => await MarkdownFilesAnalyzer.WriteResultsAsync(writer, $".{Path.DirectorySeparatorChar}WorkspaceTests");
+
+        private static void CustomAssert(string expected, string actual)
+        {
+            try
+            {
+                Assert.Equal(expected, actual, ignoreLineEndingDifferences: true);
+            }
+            catch (Exception)
+            {
+                throw new XunitException($@"
+Expected:
+{expected}
+
+Actual:
+{actual}
+");
+            }
+        }
+
+        [Fact]
+        public async Task TestMethodAsync()
+        {
+            using var workspace = new Workspace
+            {
+                Files =
+                {
+                    { "/README.md", "[text](README.md)" }
+                }
+            };
+
+            char separator = Path.DirectorySeparatorChar;
+
+            var workspacePath = await workspace.InitializeAsync();
+            using var writer = new StringWriter();
+            int returnCode = await WriteResultsAndGetExitCodeAsync(writer);
+            Assert.Equal(expected: 0, actual: returnCode);
+            CustomAssert(expected: @$"Validating links in: .{separator}WorkspaceTests{separator}README.md.
+
+", actual: writer.ToString());
+        }
     }
 }
